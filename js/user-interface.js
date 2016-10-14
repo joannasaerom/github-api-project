b508ca8af7acf0c0e6ca20454645d8ce753bb203
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-10-14 08:14:49 -0700

    Updates index.html file to include a place to display github info

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,12 +1,28 @@
 var User = require('./../js/user.js').userModule;
 
+
+function displayInfo(username, githubURL, createdDate, numOfRepos, lastUpdate){
+  $("#uName").text(username);
+  $("#ghURL").text(githubURL);
+  $("cDate").text(createdDate);
+  $("numRepos").text(numOfRepos);
+  $("#updateDate").text(lastUpdate);
+}
+
+function listRepos(repoURL){
+  for (var i = 0; i < repoURL.length; i++){
+    $("#repos").append("<li>" + repoURL[i] + "</li>");
+  }
+}
+
 $(document).ready(function() {
+  var repoURL, username, githubURL, createdDate, numOfRepos, lastUpdate;
   var newUser = new User();
   $("#searchBtn").click(function(event){
     event.preventDefault();
     var username = $("#username").val();
     $("#username").val("");
-    newUser.getRepos(username);
+    newUser.getRepos(username, displayInfo(), listRepos());
   });
 
 
