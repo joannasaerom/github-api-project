77b6791dae0b5a452d28d05da87b2e11660fbf8f
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-10-14 12:47:01 -0700

    Fixes issue where repo was not displaying properly

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,20 +1,5 @@
 var User = require('./../js/user.js').userModule;
 
-
-function displayInfo(username, githubURL, createdDate, numOfRepos, lastUpdate){
-  $("#uName").text(username);
-  $("#ghURL").text(githubURL);
-  $("cDate").text(createdDate);
-  $("numRepos").text(numOfRepos);
-  $("#updateDate").text(lastUpdate);
-}
-
-function listRepos(repoURL){
-  for (var i = 0; i < repoURL.length; i++){
-    $("#repos").append("<li>" + repoURL[i] + "</li>");
-  }
-}
-
 $(document).ready(function() {
   var repoURL, username, githubURL, createdDate, numOfRepos, lastUpdate;
   var newUser = new User();
@@ -22,7 +7,7 @@ $(document).ready(function() {
     event.preventDefault();
     var username = $("#username").val();
     $("#username").val("");
-    newUser.getRepos(username, displayInfo(), listRepos());
+    newUser.getRepos(username);
   });
 
 
--- js/user.js
+++ js/user.js
@@ -4,20 +4,31 @@ function User() {
 
 }
 
-User.prototype.getRepos = function(username, functionOne, functionTwo) {
+User.prototype.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username +'?access_token=' + apiKey).then(function(response){
     console.log(response);
 
-    var repoURL = response.repos_url;
     var username = response.login;
     var githubURL = response.html_url;
-    var createdDate = response.created_at;
-    var numOfRepos = response.public_repos;
     var lastUpdate = response.updated_at;
 
-    functionOne(username, githubURL, createdDate, numOfRepos, lastUpdate);
-    functionTwo(repoURL);
+    $("#uName").text(username);
+    $("#ghURL").text(githubURL);
+    $("#updateDate").text(lastUpdate);
 
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+  $.get('https://api.github.com/users/' + username + '/repos?&per_page=100&access_token=' + apiKey).then(function(response){
+      $("#repos").empty();
+    for (var i = 0; i < response.length; i++){
+      $("#repos").append("<h2> Repository link: " +
+                          response[i].html_url + "<br>Description: " +
+                          response[i].description + "<br>Git URL: " +
+                          response[i].clone_url + "<br>Created At: " +
+                          response[i].created_at +"</h2>");
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
