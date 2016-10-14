66e0bddba24f774945ac20c4b96d6c648dadff3d
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-10-14 07:35:09 -0700

    Assigns varibles for additional information being grabbed from API such as numer of repos and created date

--- js/user.js
+++ js/user.js
@@ -7,6 +7,18 @@ function User() {
 User.prototype.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username +'?access_token=' + apiKey).then(function(response){
     console.log(response);
+    var repoURL = response.repos_url;
+    var username = response.login;
+    var githubURL = response.html_url;
+    var createdDate = response.created_at;
+    var numOfRepos = response.public_repos;
+    var lastUpdate = response.updated_at;
+    console.log("1 " + repoURL);
+    console.log("2 " + username);
+    console.log("3 " + githubURL);
+    console.log("4 " + createdDate);
+    console.log("5 " + numOfRepos);
+    console.log("6 " + lastUpdate);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
