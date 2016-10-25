7c0e367df0f184e5b8ac8a36f3ae5100fac8881e
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-10-24 18:10:35 -0700

    Separates jQuery from backend

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,5 +1,22 @@
 var User = require('./../js/user.js').userModule;
 
+function displayUserInfo(username, githubURL, lastUpdate){
+  $("#userInfo").show();
+  $("#uName").text(username);
+  $("#ghURL").html("<a href='" + githubURL + "'>" + githubURL + "</a>");
+  $("#updateDate").text(lastUpdate);
+}
+
+function displayRepoInfo(userRepos){
+  for (var i = 0; i < userRepos.length; i++){
+    $("#repos").append("<hr><h2> Repository name: " + userRepos[i].name +
+                        "<br><a href='" + userRepos[i].html_url + "'>" + "Repository link</a> " +
+                        "<br>Description: " + userRepos[i].description +
+                        "<br>Created on: " + userRepos[i].created_at +
+                        "<br>Run the following command to clone: $ git clone " + userRepos[i].clone_url +"</h2>");
+  }
+}
+
 $(document).ready(function() {
   var repoURL, username, githubURL, createdDate, numOfRepos, lastUpdate;
   var newUser = new User();
@@ -7,6 +24,6 @@ $(document).ready(function() {
     event.preventDefault();
     var username = $("#username").val();
     $("#username").val("");
-    newUser.getRepos(username);
+    newUser.getRepos(username, displayUserInfo, displayRepoInfo);
   });
 });
--- js/user.js
+++ js/user.js
@@ -4,17 +4,14 @@ function User() {
 
 }
 
-User.prototype.getRepos = function(username) {
+User.prototype.getRepos = function(username, displayUserInfo, displayRepoInfo) {
   $.get('https://api.github.com/users/' + username +'?access_token=' + apiKey).then(function(response){
     console.log(response);
 
     var username = response.login;
     var githubURL = response.html_url;
     var lastUpdate = response.updated_at;
-    $("#userInfo").show();
-    $("#uName").text(username);
-    $("#ghURL").html("<a href='" + githubURL + "'>" + githubURL + "</a>");
-    $("#updateDate").text(lastUpdate);
+    displayUserInfo(username, githubURL, lastUpdate);
 
   }).fail(function(error){
     console.log(error.responseJSON.message);
@@ -22,13 +19,16 @@ User.prototype.getRepos = function(username) {
 
   $.get('https://api.github.com/users/' + username + '/repos?&per_page=100&access_token=' + apiKey).then(function(response){
       $("#repos").empty();
-    for (var i = 0; i < response.length; i++){
-      $("#repos").append("<hr><h2> Repository name: " + response[i].name +
-                          "<br><a href='" + response[i].html_url + "'>" + "Repository link</a> " +
-                          "<br>Description: " + response[i].description +
-                          "<br>Created on: " + response[i].created_at +
-                          "<br>Run the following command to clone: $ git clone " + response[i].clone_url +"</h2>");
-    }
+
+      displayRepoInfo(response);
+
+    // for (var i = 0; i < response.length; i++){
+    //   $("#repos").append("<hr><h2> Repository name: " + response[i].name +
+    //                       "<br><a href='" + response[i].html_url + "'>" + "Repository link</a> " +
+    //                       "<br>Description: " + response[i].description +
+    //                       "<br>Created on: " + response[i].created_at +
+    //                       "<br>Run the following command to clone: $ git clone " + response[i].clone_url +"</h2>");
+    // }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
